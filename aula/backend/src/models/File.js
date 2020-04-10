4e9168319e225f364a7485504823bb56bcc373d1
leocalmeida <leonardocaetanoalmeida@gmail.com>
2020-04-10 10:09:54 -0300

    Adiciona variaveis ambientes

--- aula/backend/src/models/File.js
+++ aula/backend/src/models/File.js
@@ -13,13 +13,15 @@ const File = new mongoose.Schema(
   },
   {
     timestamps: true,
-    toObject: {virtuals: true},
-    toJSON: {virtuals: true}
+    toObject: { virtuals: true },
+    toJSON: { virtuals: true },
   }
 );
 
 File.virtual("url").get(function () {
-  return `http://localhost:3333/files/${encodeURIComponent(this.path)}`;
-})
+  const url = process.env.URL || "http://localhost:3333";
+
+  return `${url}/files/${encodeURIComponent(this.path)}`;
+});
 
 module.exports = mongoose.model("File", File);
