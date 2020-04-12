7f50fae15a832be83bf98d5d3691b35540bd9367
leocalmeida <leonardocaetanoalmeida@gmail.com>
2020-04-12 08:42:12 -0300

    atualizacao cors

--- src/app.js
+++ src/app.js
@@ -7,7 +7,9 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000'
+}));
 
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
