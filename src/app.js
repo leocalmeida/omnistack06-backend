ac6efec5984909969e218ef1d61a291cb1c10fa5
leocalmeida <leonardocaetanoalmeida@gmail.com>
2020-05-17 09:19:22 -0300

    Aplicando criptografia no arquivo de conexao

--- src/app.js
+++ src/app.js
@@ -1,34 +1,32 @@
 const express = require("express");
 const routes = require("./routes");
 const mongoose = require("mongoose");
+const mongoConnect = require("./config/mongoConnect");
 const path = require("path");
 const cors = require("cors");
-
 
 const app = express();
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header("Access-Control-Allow-Origin", "*");
   next();
 });
 
 app.use(cors());
 
 const server = require("http").Server(app);
-const io = require("socket.io")(server, {origins: "*:*"});
+const io = require("socket.io")(server, { origins: "*:*" });
 
-io.on("connection", socket => {
-  socket.on("connectRoom", box => {
+io.on("connection", (socket) => {
+  socket.on("connectRoom", (box) => {
     socket.join(box);
   });
 });
 
-mongoose.connect(
-  "mongodb+srv://omnistack:lcaajj21@cluster0-ll3m3.mongodb.net/omnistack06?retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true, useUnifiedTopology: true,
-  }
-);
+mongoose.connect(mongoConnect, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.use((req, res, next) => {
   req.io = io;
@@ -39,7 +37,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/files", express.static(path.resolve(__dirname,"..","tmp")));
+app.use("/files", express.static(path.resolve(__dirname, "..", "tmp")));
 app.use(routes);
 
 module.exports = server;
